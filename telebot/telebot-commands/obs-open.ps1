ef9ba0c3123586bfe795c563a156a630140ed441
hmrs-cr <hmrodriguez@gmail.com>
2020-08-02 13:32:56 -0600

    Check if obs is alredy running

--- telebot/telebot-commands/obs-open.ps1
+++ telebot/telebot-commands/obs-open.ps1
@@ -1,13 +1,18 @@
 $obsProcName = "obs64"
 $obspath="C:\Program Files\obs-studio\bin\64bit\$obsProcName.exe"
 
-Log "Opening $obspath"
-Start-Process -FilePath $obspath -WorkingDirectory $(Split-Path -Path $obspath)
-Start-Sleep 2
-
 $obsProcess = $(Get-Process -ErrorAction:Ignore $obsProcName)
-if($obsProcess) {
-    Reply -Message "OBS is now running."
+if ($obsProcess) {
+    Reply -Message "OBS already running."
 } else {
-    Reply -Message "OBS is not running."
+    Log "Opening $obspath"
+    Start-Process -FilePath $obspath -WorkingDirectory $(Split-Path -Path $obspath)
+    Start-Sleep 2
+
+    $obsProcess = $(Get-Process -ErrorAction:Ignore $obsProcName)
+    if($obsProcess) {
+        Reply -Message "OBS is now running."
+    } else {
+        Reply -Message "OBS is not running."
+    }
 }
