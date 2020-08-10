41a81b57a02c627b17b243ea88e94f6d34a5a037
hmrs-cr <hmrodriguez@gmail.com>
2020-08-09 20:53:15 -0600

    Update lock.ps1

--- telebot/telebot-commands/lock.ps1
+++ telebot/telebot-commands/lock.ps1
@@ -1,3 +1,14 @@
 param([string] $Params, [int]$messageId)
-
 rundll32.exe user32.dll,LockWorkStation
+
+$count = 0;
+while (-not ($process = $(Get-Process -ErrorAction:Ignore logonui)) -and $count -le 5) {
+    Start-Sleep 1
+    $count = $count + 1
+}
+
+if ($process) {
+    Reply -Message "Screen is locked." -ReplyToId $messageId
+} else {
+    Reply -Message "Screen is NOT locked!" -ReplyToId $messageId
+}
